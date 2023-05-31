4df474d5d1c4c7d6c6f591532aac1569764dc9bc
sevi78 <125169048+sevi78@users.noreply.github.com>
2023-05-31 23:09:58 +0200

    initial cleanup

--- source/texts.py
+++ source/texts.py
@@ -22,7 +22,7 @@ Special: The water vapor of the planet seems to be hallucinogenic and slightly a
 Name: Kepler-22b:\n
 Type: Earthlike\n
 Raw materials: minerals and energy\n
-Residents: Sex addicts with 3 penises and/or up to 4 breasts, mostly two vaginas. Often a few crew members stay there forever, the rest bring at least one STD on board.\n
+Residents: unknown Species\n
 Special: None\n
 """,
 
