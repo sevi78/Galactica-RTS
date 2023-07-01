33e7b0c82504cd64af718c8b2075e92729055004
severin stirnemann <severinstirnemann@gmail.com>
2023-07-01 21:03:50 +0200

    Delete source directory

--- source/unused/pan_zoom_test.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-from pan_zoom import Panzoom
-
-fig1 = plt.figure()
-plt.plot([0, 1, 3, 6, 7, 4, 2, 0])
-plt.show()
-panzoomer = Panzoom(fig1)
-print("drag-n-drop panning and mouswheel zooming awesomeness. Now: at your fingertips!")
